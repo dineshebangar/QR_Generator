f583c766e269a7b378198573214301aede2aab45
Dinesh Bangar <dinesh.bangar@ilfstechnologies.com>
2014-07-01 20:49:54 +0530

    commit all

--- app/helpers/qrcodes_helper.rb
+++ app/helpers/qrcodes_helper.rb
@@ -3,15 +3,22 @@ module QrcodesHelper
 
   def render_qr_code text, size = 3
     return if text.to_s.empty?
-    qr = RQRCode::QRCode.new(text, :size => size, :level => :h)
-    # while size < 10
-    #   begin
-    #     qr = RQRCode::QRCode.new(text, :size => size, :level => :l)
-    #   rescue RQRCode::QRCodeRunTimeError => e
-    #     size += 1
-    #   end
-    # end
-    sizeStyle = "width: #{size}px; height: #{size}px;"
+    #qr = RQRCode::QRCode.new(text, :size => size, :level => :h, typeNumber: 20, correctLevel: 0)
+    
+    size_img = 2 if size === 6
+    size_img = 3 if size === 8
+    size_img = 4 if size === 10
+    qr = nil
+    while qr == nil && size < 30
+      begin
+        qr = RQRCode::QRCode.new(text, :size => size, :level => :h)
+        rescue RQRCode::QRCodeRunTimeError => e
+        size += 1
+      end
+    end
+    
+    sizeStyle = "width: #{size_img}px; height: #{size_img}px;"
+    #sizeStyle = "width: #{size}px; height: #{size}px;"
     content_tag :table, class: "qrcode pull-right" do
       qr.modules.each_index do |x|
         concat(content_tag(:tr) do
